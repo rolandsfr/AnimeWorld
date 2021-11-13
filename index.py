bb416255208a23355f8beab0acb3ce924670a8d5
Ammar <amarftw1@gmail.com>
2021-11-13 16:45:46 +0100

    [patch] follow PEP8 convention for names; snake_case

--- index.py
+++ index.py
@@ -3,40 +3,41 @@ import os
 import requests
 import random
 
+
 # Defining utility class to later validate values and types of input data
 class Types:
-    def _anyParser(self, val):
+    def _any_parser(self, val):
         return len(val) != 0
 
     def __init__(self):
-        self.parsers = [{"any": self._anyParser}]
+        self.parsers = [{"any": self._any_parser}]
 
     def validate(self, value, type):
         # First of all validate the any type (non empty)
-        nonEmpty = self._anyParser(value)
-        if nonEmpty == False:
+        non_empty = self._any_parser(value)
+        if non_empty == False:
             raise ValueError("Please, enter a value")
 
         for parser in self.parsers:
             for key in parser.keys():
                 if key == type:
-                    isValidOrException = parser[key](value)
-                    if isValidOrException != True:
-                        raise ValueError(isValidOrException)
+                    is_valid_or_exception = parser[key](value)
+                    if is_valid_or_exception != True:
+                        raise ValueError(is_valid_or_exception)
 
-    def add(self, type, parserFunc):
+    def add(self, type, parser_func):
         # parser function returns boolean whether or not the value was parsed successfully
-        self.parsers.append({type: parserFunc})
+        self.parsers.append({type: parser_func})
 
 
-def ynParser(val):
+def yn_parser(val):
     if val == "y" or val == "n" or val == "yes" or val == "no":
         return True
     else:
         return ValueError(f"Value {val} is not assignable to type yes/no/y/n")
 
 
-def animeParser(val):
+def anime_parser(val):
     ALLOWED = [
         "naruto",
         "one piece",
@@ -55,11 +56,12 @@ def animeParser(val):
 
 # initializing utility types
 types = Types()
-types.add("yn", ynParser)
-types.add("anime", animeParser)
+types.add("yn", yn_parser)
+types.add("anime", anime_parser)
+
 
 # Looping on the incorrect values
-def inputWithValidation(query, type="any"):
+def input_with_validation(query, type="any"):
     value = ""
     loop = True
 
@@ -74,34 +76,34 @@ def inputWithValidation(query, type="any"):
     return value
 
 
-def getRandomFact(anime):
+def get_random_fact(anime):
     print("\nFetching an external resource...")
     response = requests.get(
         f"https://anime-facts-rest-api.herokuapp.com/api/v1/{anime}"
     )
     data = response.json()["data"]
-    randomId = random.randint(1, len(data))
-    funFact = ""
+    random_id = random.randint(1, len(data))
+    fun_fact = ""
 
     for fact in data:
-        if fact["fact_id"] == randomId:
-            funFact = fact["fact"]
+        if fact["fact_id"] == random_id:
+            fun_fact = fact["fact"]
 
-    return funFact
+    return fun_fact
 
 
-def downloadAnime(fileName):
+def download_anime(fileName):
     print("\nDownloading anime image...")
     response = requests.get("https://api.waifu.pics/sfw/poke")
     url = response.json()["url"]
-    imageResponse = requests.get(url)
+    image_response = requests.get(url)
 
     # create a file and subdirectory
     if os.path.exists("assets") != True:
         os.mkdir("assets")
 
     file = open("assets/" + fileName, "wb")
-    file.write(imageResponse.content)
+    file.write(image_response.content)
     file.close()
 
     print(
@@ -109,17 +111,17 @@ def downloadAnime(fileName):
     )
 
 
-def initApp():
-    completeName = "user.json"
-    animeFileName = "anime.gif"
+def init_app():
+    complete_name = "user.json"
+    anime_file_name = "anime.gif"
 
-    def initializeUser():
+    def initialize_user():
         print(
             "Hello, Welcome to AnimeWorld!\nWe first need to gather some information before we can start."
         )
-        name = inputWithValidation("Whats your name? ")
-        fav = inputWithValidation("What is your favorite anime? ", "anime")
-        additional = inputWithValidation(
+        name = input_with_validation("Whats your name? ")
+        fav = input_with_validation("What is your favorite anime? ", "anime")
+        additional = input_with_validation(
             "Do you want to receive a random anime image on startup? yes/no/y/n ", "yn"
         )
 
@@ -129,33 +131,33 @@ def initApp():
             "sendImage": True if additional == "yes" or additional == "y" else False,
         }
 
-        with open(completeName, "w+") as f:
+        with open(complete_name, "w+") as f:
             json.dump(config, f)
 
         return config
 
-    def startWork(config):
+    def start_work(config):
         # greet the user
         name = config["name"]
         favorite = config["favorite"].replace(" ", "_")
         print(f"Welcome back, {name}!")
 
         # fetch facts about the favorite anime
-        fact = getRandomFact(favorite).replace(".", "")
+        fact = get_random_fact(favorite).replace(".", "")
         print(
             f"\n\nHere's a random fact about your favorite anime `{favorite}`!\n - Did you know that {fact}?\n"
         )
 
         # download an anime fig
-        downloadAnime(animeFileName)
+        download_anime(anime_file_name)
 
     try:
-        initialConfig = open(completeName, "r")
-        parsedConfig = json.loads(initialConfig.read())
-        startWork(parsedConfig)
+        initial_config = open(complete_name, "r")
+        parsed_config = json.loads(initial_config.read())
+        start_work(parsed_config)
     except Exception as e:
-        config = initializeUser()
-        startWork(config)
+        config = initialize_user()
+        start_work(config)
 
 
-initApp()
+init_app()
